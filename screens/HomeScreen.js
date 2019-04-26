0c723dca894cf7e00982da885fd21fca47e7f23e
Alexander Dubois <sander.dubois@outlook.com>
2019-04-26 10:54:04 +0100

    Added Search, UI for profile imgae and average star⭐️

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,6 +10,8 @@ import {
   View,
 } from 'react-native';
 
+import { SearchBar, ListItem } from 'react-native-elements';
+
 BASE_URL = 'http://localhost:3000'
 PRACTITIONERS_URL = BASE_URL + '/practitioners'
 
@@ -17,7 +19,8 @@ PRACTITIONERS_URL = BASE_URL + '/practitioners'
 export default class HomeScreen extends React.Component {
 
   state = {
-    practitioners: []
+    practitioners: [],
+    searchTerm: ""
   }
 
   componentDidMount() {
@@ -30,17 +33,49 @@ export default class HomeScreen extends React.Component {
         })}) 
   }
 
+  handleSearchChange = (searchTerm) => {
+    this.setState({
+      searchTerm
+    })
+  }
+
+  filteredPractitioners = () => {
+    const {practitioners, searchTerm} = this.state
+    return practitioners.filter((practitioner) => practitioner.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  }
+
+  renderListItems = ({item}) => (
+    <ListItem
+      key={item.id}
+      leftAvatar={{ source: { uri: item.profile_img } }}
+      title={item.name}
+      badge={{value: `Average star: ${this.getAverageReview(item)}`}}
+    />
+  )
+
+  getAverageReview = (practitioner) => {
+    const {reviews} = practitioner
+    if (reviews.length === 0) return 0
+
+    let totalNumberOfStars = 0
+    reviews.forEach((review) => totalNumberOfStars += review.star)
+    return (totalNumberOfStars / reviews.length)
+  }
+
   render() {
+    const {searchTerm} = this.state
+
     return (
       <View style={styles.container}>
+        <SearchBar
+          placeholder="Search..."
+          lightTheme
+          onChangeText={this.handleSearchChange}
+          value={searchTerm}/>
         <FlatList
-          data={this.state.practitioners} 
+          data={this.filteredPractitioners()} 
           keyExtractor={({id}) => `${id}`}
-          renderItem={({item}) => (
-            <View style={{backgroundColor: blue}}>
-              <Text>{item.name}</Text> 
-            </View>
-          )}/>
+          renderItem={this.renderListItems}/>
       </View>
     );
   }
